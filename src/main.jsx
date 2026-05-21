bcfe7f0f6198a2bf8c75e584fea151c09b4fb642
JG <joaogabrielrsousa07@gmail.com>
2026-05-21 16:42:05 -0300

    Consertando o erro da cor de fundo não acompanhar a página

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider } from "react-router-dom";
 import TaskPage from "./pages/TaskPage.jsx";
+import "../index.css";
 
 const router = createBrowserRouter([
     {
